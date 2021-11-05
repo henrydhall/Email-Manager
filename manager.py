6ca5670067862dd258e8590d5ce95cca9472aaab
Henry Hall <henrydh@byu.edu>
2021-11-05 14:51:11 -0600

    added functionality to accept file to read users to delete emails from

--- manager.py
+++ manager.py
@@ -1,10 +1,9 @@
 import ezgmail
-import os
+import os,sys
 
 def searchBySender(senderToSearch):
-    print('Searching for emails...')
-    sendersToSearch = ['Facebook','Amazon.com','Amazon Prime','Robinhood Snacks']
-    searchedEmails = ezgmail.search(maxResults=2000, query=f'from:{senderToSearch}' )
+    print(f'Searching for emails from {senderToSearch}...')
+    searchedEmails = ezgmail.search(maxResults=500, query=f'from:{senderToSearch}' )
     return searchedEmails
 
 def deleteEmails(emailsToDelete):
@@ -12,10 +11,27 @@ def deleteEmails(emailsToDelete):
     for email in emailsToDelete:
         email.trash()
 
-if __name__ == '__main__':
+def deleteByUsers(fileToRead):
     ezgmail.init()
-    senderToDelete = 'Netflix'
-    searchedEmails = searchBySender(senderToDelete)
-    while(len(searchedEmails)) > 0:
-        deleteEmails(searchedEmails)
+    f = open(fileToRead)
+    sendersToDelete = f.readlines()
+    f.close()
+    for line in sendersToDelete:
+        if line in ['','\n']:
+            raise ValueError('Empty line error.')
+        if len(line) < 5:
+            userInput = 'nopenotyet'
+            while userInput != 'y':
+                userInput = input(f'Short input: {line}. Enter \'y\' to proceed.')
+                if userInput == 'y':
+                    break
+        senderToDelete = line.strip()
         searchedEmails = searchBySender(senderToDelete)
+        while len(searchedEmails) > 0:
+            deleteEmails(searchedEmails)
+            searchedEmails = searchBySender(senderToDelete)
+
+
+if __name__ == '__main__':
+    fileToRead = sys.argv[1]
+    deleteByUsers(fileToRead)
